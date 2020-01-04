ecfa665fae200581a4a3c0057674fbf9aeaa0df3
raimund89 <raimund89@users.noreply.github.com>
2020-01-04 19:40:33 +0100

    Cleaning up a bit

--- ESP32-Activity-Logger.ino
+++ ESP32-Activity-Logger.ino
@@ -3,38 +3,16 @@
 #include "Bluetooth.h"
 
 TinyGPSPlus gps;
-
-// Using Neil Kolban's Bluetooth libraries. Can save a lot of space on the device by not doing this...
-// Bluetooth specs for GATT at https://www.bluetooth.com/specifications/gatt/characteristics/
-
-// TODO: Add more callback methods
-// TODO: Add support for heart rate AND cadence
-// TODO: Somehow be able to spawn multiple BT connections
+Bluetooth* btConnection;
+bool btUpdate = false;
 
 static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  // **** For Heart Rate 0x2A37 ****
-  // pData[0] contains the flags of the transmission [format, status(2), energy expenditure, RR-interval length]
-  // pData[1] contains the heart rate in 8-bit or 16-bit depending on the flags
-  // pData[2:] contain energy expenditure (1 value) and RR values (sets of 2), depending on the flags
-  // **** For Body Location 0x2A38 ****
-  // pData[0] sensor location, choice of Other, Chest, Wrist, Finger, Hand, Ear Lobe, Foot
-  // **** For Cadence 0x2A5B ****
-  // pData[0] contains the flags of the transmission [wheel revolution, crank revolution]
-  // pdata[1] cumulative wheel revolutions
-  // pData[2] last wheel event time
-  // pData[3] cumulative crank revolutions
-  // pData[4] last crank event time
-  // **** For Cadence Feature 0x2A5C ****
-  // pData[0] contains the feature fields [wheel revolution, crank revolution, multiple sensor locations]
   Serial.println(pData[1]);
 }
 
 static void messageCallback(const char* message) {
   Serial.println(message);
 }
-
-Bluetooth* btConnection;
-bool btUpdate = false;
 
 // For the Bluetooth connection, which only needs to be
 // called every few seconds, give or take, to keep it alive.
@@ -70,7 +48,10 @@ void loop() {
   }
 
   if(gps.location.isUpdated()) {
-    Serial.print("Latitude = ");
+    Serial.print("Fix time = ");
+    Serial.print(gps.date.value());
+    Serial.print(gps.time.value());
+    Serial.print(",Latitude = ");
     Serial.print(gps.location.lat(),8);
     Serial.print(", Longitude = ");
     Serial.println(gps.location.lng(),8);
--- README.md
+++ README.md
@@ -19,3 +19,21 @@ The project uses Neil Kolban's libraries for Bluetooth connections ([here](https
 - [ ] Increase GPS capabilities (time, speed, etc)
 - [ ] Increase Bluetooth capabilities (battery, etc)
 - [ ] In circuit, add an on/off switch, battery, RTC
+
+## Some background
+Just to record the basics of GATT for the software. Don't mind this section, they are kind of random notes...
+
+// **** For Heart Rate 0x2A37 ****
+// pData[0] contains the flags of the transmission [format, status(2), energy expenditure, RR-interval length]
+// pData[1] contains the heart rate in 8-bit or 16-bit depending on the flags
+// pData[2:] contain energy expenditure (1 value) and RR values (sets of 2), depending on the flags
+// **** For Body Location 0x2A38 ****
+// pData[0] sensor location, choice of Other, Chest, Wrist, Finger, Hand, Ear Lobe, Foot
+// **** For Cadence 0x2A5B ****
+// pData[0] contains the flags of the transmission [wheel revolution, crank revolution]
+// pdata[1] cumulative wheel revolutions
+// pData[2] last wheel event time
+// pData[3] cumulative crank revolutions
+// pData[4] last crank event time
+// **** For Cadence Feature 0x2A5C ****
+// pData[0] contains the feature fields [wheel revolution, crank revolution, multiple sensor locations]

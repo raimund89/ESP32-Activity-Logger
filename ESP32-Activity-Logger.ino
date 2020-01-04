21f70f696a9e6e0152cfa885c3f72aa14c61abe4
raimund89 <raimund89@users.noreply.github.com>
2020-01-04 02:00:49 +0100

    Add some info as my own quick cheat sheet

--- ESP32-Activity-Logger.ino
+++ ESP32-Activity-Logger.ino
@@ -6,9 +6,15 @@
 // Using Neil Kolban's Bluetooth libraries. Can save a lot of space on the device by not doing this...
 // Bluetooth specs for GATT at https://www.bluetooth.com/specifications/gatt/characteristics/
 
-// Any heart rate device supporting GATT will be at UUID 0x180D
+// **** GATT service UUIDs ****
+// 0x180D Heart Rate
+// 0x1814 Running Speed and Cadence
 static BLEUUID serviceUUID((uint16_t)0x180D);
-// Any heart rate device supporting GATT will have it's heart rate value at 0x2A37
+// **** GATT characteristic UUIDs ****
+// 0x2A37 Heart Rate Measurement
+// 0x2A38 Body Location
+// 0x2A5B Running Speed and Cadence
+// 0x2A5C Running Speed and Cadence Feature
 static BLEUUID      hrUUID((uint16_t)0x2A37);
 
 // The scan device
@@ -25,9 +31,20 @@ static boolean doScan = false;
 static BLERemoteCharacteristic* pRemoteCharacteristic;
 
 static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  // pData[0] contains the flags of the transmission
+  // **** For Heart Rate 0x2A37 ****
+  // pData[0] contains the flags of the transmission [format, status(2), energy expenditure, RR-interval length]
   // pData[1] contains the heart rate in 8-bit or 16-bit depending on the flags
   // pData[2:] contain energy expenditure (1 value) and RR values (sets of 2), depending on the flags
+  // **** For Body Location 0x2A38 ****
+  // pData[0] sensor location, choice of Other, Chest, Wrist, Finger, Hand, Ear Lobe, Foot
+  // **** For Cadence 0x2A5B ****
+  // pData[0] contains the flags of the transmission [wheel revolution, crank revolution]
+  // pdata[1] cumulative wheel revolutions
+  // pData[2] last wheel event time
+  // pData[3] cumulative crank revolutions
+  // pData[4] last crank event time
+  // **** For Cadence Feature 0x2A5C ****
+  // pData[0] contains the feature fields [wheel revolution, crank revolution, multiple sensor locations]
   Serial.println(String("Heart rate: ") + pData[1]);
 }
 
